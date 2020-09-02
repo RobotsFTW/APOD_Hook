3cb750962a100bfaa1044ef26d8f8c726e65df7a
James White <bluepillftw@gmail.com>
2020-09-02 15:07:55 -0500

    formatting changes

--- webhook.py
+++ webhook.py
@@ -1,10 +1,11 @@
-import requests
-import discord
-from discord import Webhook, RequestsWebhookAdapter, File
-import textwrap
 import datetime
 import json
-#python config file with the API keys in it.
+import textwrap
+
+import discord
+import requests
+from discord import RequestsWebhookAdapter, Webhook
+
 import config
 
 #Webhook to send astronomy picture of the day
@@ -14,10 +15,7 @@ webhook = Webhook.from_url(config.WEBHOOK_URL, adapter=RequestsWebhookAdapter())
 
 #Get todays date and format it for the APOD API URL
 now=datetime.datetime.today()
-YYYY=now.year
-MM=now.month
-DD=now.day
-date="{}-{}-{}".format(YYYY, MM, DD)
+date="{}-{}-{}".format(now.year, now.month, now.day)
 
 #insert date and api key into the NASA APOD API URL
 url = "https://api.nasa.gov/planetary/apod?api_key={}&date={}".format(config.NASA_KEY, date)
@@ -26,49 +24,33 @@ url = "https://api.nasa.gov/planetary/apod?api_key={}&date={}".format(config.NAS
 res = requests.get(url)
 data=res.json()
 
-#check to see if there is an error, can happen if cheching the
-#picture to early in the day.
+#check to see if there is an error. Send error and Exit.
 if "code" in data:
     msg = data["msg"]
     webhook.send(msg)
-elif data["media_type"] == "image":
-    title= data['title']
-    date = data['date']
-    descr = data['explanation']
+    exit()
+
+#create the discord embed, add title
+title= data['title']
+embed = discord.Embed(title="Astronomy Picture of the Day for {}.".format(date), description=title, color=0x0B3D91)
+
+#textwrap it so it fits within discord message length
+descr = data['explanation']
+content = textwrap.wrap(descr, 1000)
+for item in content:
+    embed.add_field(name="Description:", value=item, inline=False)
+
+if data["media_type"] == "image":
+    #adds the images to teh embed
     hd_img = data['hdurl']
     sd_img = data['url']
-
-    #create the discord embed, and add the SD image
-    embed = discord.Embed(title="Astronomy Picture of the Day for {}.".format(date), description=title, color=0x0B3D91)
     embed.set_image(url=sd_img)
-
-    #textwrap it so it fits within discord message length
-    content = textwrap.wrap(descr, 1000)
-    for item in content:
-        embed.add_field(name="Description:", value=item, inline=False)
-
-    #Add image links
     embed.add_field(name="HD Image Link:", value=hd_img, inline=False)
     embed.add_field(name="SD Image Link:", value=sd_img, inline=False)
-
-    # Send embed to server
-    webhook.send(embed=embed)
 elif data["media_type"] == "video":
-    title= data['title']
-    date = data['date']
-    descr = data['explanation']
+    #Adds teh video link to the embed
     vid_url = data["url"]
-
-    #create the discord embed, and add the video Link
-    embed = discord.Embed(title="Astronomy Picture of the Day for {}.".format(date), description=title, color=0x0B3D91)
-
-    #textwrap it so it fits within discord message length
-    content = textwrap.wrap(descr, 1000)
-    for item in content:
-        embed.add_field(name="Description:", value=item, inline=False)
-
-    #Add Video Link
     embed.add_field(name="Video Link:", value=vid_url, inline=False)
 
-    #send Webhook
-    webhook.send(embed=embed)
+#send Webhook
+webhook.send(embed=embed)
