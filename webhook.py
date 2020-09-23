9b67e69ea82676741c454f5d2985762c3795a9a0
James White <bluepillftw@gmail.com>
2020-09-23 13:30:52 -0500

    spelling error correction

--- webhook.py
+++ webhook.py
@@ -1,5 +1,4 @@
 import datetime
-import json
 import textwrap
 
 import discord
@@ -14,15 +13,14 @@ import config
 webhook = Webhook.from_url(config.WEBHOOK_URL, adapter=RequestsWebhookAdapter())
 
 #Get todays date and format it for the APOD API URL
-now=datetime.datetime.today()
+now = datetime.datetime.today()
 date="{}-{}-{}".format(now.year, now.month, now.day)
 
 #insert date and api key into the NASA APOD API URL
 url = "https://api.nasa.gov/planetary/apod?api_key={}&date={}".format(config.NASA_KEY, date)
 
 #Get information from response to embed in message
-res = requests.get(url)
-data=res.json()
+data = requests.get(url).json()
 
 #check to see if there is an error. Send error and Exit.
 if "code" in data:
@@ -31,24 +29,23 @@ if "code" in data:
     exit()
 
 #create the discord embed, add title
-title= data['title']
+title = data['title']
 embed = discord.Embed(title="Astronomy Picture of the Day for {}.".format(date), description=title, color=0x0B3D91)
 
 #textwrap it so it fits within discord message length
 descr = data['explanation']
-content = textwrap.wrap(descr, 1000)
-for item in content:
+for item in textwrap.wrap(descr, 1000):
     embed.add_field(name="Description:", value=item, inline=False)
 
 if data["media_type"] == "image":
-    #adds the images to teh embed
+    #adds the images to the embed
     hd_img = data['hdurl']
     sd_img = data['url']
     embed.set_image(url=sd_img)
     embed.add_field(name="HD Image Link:", value=hd_img, inline=False)
     embed.add_field(name="SD Image Link:", value=sd_img, inline=False)
 elif data["media_type"] == "video":
-    #Adds teh video link to the embed
+    #Adds the video link to the embed
     vid_url = data["url"]
     embed.add_field(name="Video Link:", value=vid_url, inline=False)
 
--- webhook.sh
+++ webhook.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#This .sh script is so we can lauch the web hook from a 
+#This script is so we can launch the web hook from a 
 #crontab everyday at a certain time
 python3 ~/APOD_Hook/webhook.py
 
