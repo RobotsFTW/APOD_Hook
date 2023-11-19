ddf645d219daa21efce95055adcb7dc3d159ff61
James White <bluepillftw@gmail.com>
2023-11-18 19:18:21 -0600

    Added retries to sending the APOD.

--- webhook.py
+++ webhook.py
@@ -1,67 +1,81 @@
 import datetime
 import textwrap
 import logging
+import time
 import traceback
 
 import discord
 import requests
-from discord import SyncWebhook
 
+# user settings
 import config
 
-#Webhook to send astronomy picture of the day
+# Webhook to send astronomy picture of the day
 
-#logging
-log_fname = config.log_file
-logging.basicConfig(filename=log_fname, level=logging.INFO)
+# logging
+log_file_name = config.log_file
+logging.basicConfig(filename=log_file_name, level=logging.INFO)
 
-#put it all in a try except statment to log any errors. i.e. no internet connection
-try:
-    #Create webhook
-    webhook = SyncWebhook.from_url(config.WEBHOOK_URL)
+# put it all in a try except statement to log any errors. i.e. no internet connection
+retries = 1
+APOD_sent = False
 
-    #Get todays date and format it for the APOD API URL
-    now = datetime.datetime.today()
-    date = "{}-{}-{}".format(now.year, now.month, now.day)
+# Tries to send the APOD 5 times.
+while not APOD_sent and retries < 5:
+    try:
+        # Create webhook
+        webhook = discord.SyncWebhook.from_url(config.WEBHOOK_URL)
 
-    #insert date and api key into the NASA APOD API URL
-    url = "https://api.nasa.gov/planetary/apod?api_key={}&date={}".format(config.NASA_KEY, date)
+        # Get today's date and format it for the APOD API URL
+        now = datetime.datetime.today()
+        date = "{}-{}-{}".format(now.year, now.month, now.day)
 
-    #Get information from response to embed in message
-    data = requests.get(url).json()
+        # insert date and api key into the NASA APOD API URL
+        url = "https://api.nasa.gov/planetary/apod?api_key={}&date={}".format(config.NASA_KEY, date)
 
-    #check to see if there is an error. Send error and Exit.
-    if "code" in data:
-        msg = data["msg"]
-        webhook.send(msg)
-        exit()
+        # Get information from response to embed in message
+        data = requests.get(url).json()
 
-    #create the discord embed, add title
-    title = data['title']
-    embed = discord.Embed(title="Astronomy Picture of the Day for {}.".format(date), description=title, color=0x0B3D91)
+        # check to see if there is an error. Send error and Exit.
+        if "code" in data:
+            msg = data["msg"]
+            webhook.send(msg)
+            exit()
 
-    #textwrap it so it fits within discord message length
-    descr = data['explanation']
-    for item in textwrap.wrap(descr, 1000):
-        embed.add_field(name="Description:", value=item, inline=False)
+        # create the discord embed, add title
+        title = data['title']
+        embed = discord.Embed(title="Astronomy Picture of the Day for {}.".format(date), description=title,
+                              color=0x0B3D91)
 
-    if data["media_type"] == "image":
-        #adds the images to the embed
-        hd_img = data['hdurl']
-        sd_img = data['url']
-        embed.set_image(url=sd_img)
-        embed.add_field(name="HD Image Link:", value=hd_img, inline=False)
-        embed.add_field(name="SD Image Link:", value=sd_img, inline=False)
-    elif data["media_type"] == "video":
-        #Adds the video link to the embed
-        vid_url = data["url"]
-        embed.add_field(name="Video Link:", value=vid_url, inline=False)
+        # textwrap it so it fits within discord message length
+        descr = data['explanation']
+        for item in textwrap.wrap(descr, 1000):
+            embed.add_field(name="Description:", value=item, inline=False)
 
-    #send Webhook
-    webhook.send(embed=embed)
+        if data["media_type"] == "image":
+            # adds the images to the embed
+            hd_img = data['hdurl']
+            sd_img = data['url']
+            embed.set_image(url=sd_img)
+            embed.add_field(name="HD Image Link:", value=hd_img, inline=False)
+            embed.add_field(name="SD Image Link:", value=sd_img, inline=False)
+        elif data["media_type"] == "video":
+            # Adds the video link to the embed
+            vid_url = data["url"]
+            embed.add_field(name="Video Link:", value=vid_url, inline=False)
 
-    #log successfully sent APOD
-    logging.info(f'APOD Successfully sent: {now}')
-except Exception as e:
-    logging.error(f'{datetime.datetime.now()}: {traceback.format_exc()}')
+        # send Webhook
+        webhook.send(embed=embed)
 
+        # log successfully sent APOD
+        logging.info(f'APOD Successfully sent: {now}')
+
+        # create success
+        APOD_sent = True
+    # logs error and tries to resend it up to five times. Waiting 1 minute longer each time.
+    except Exception as e:
+        logging.error(f'{datetime.datetime.now()}: {traceback.format_exc()}')
+        print(e)
+        wait = retries * 60
+        time.sleep(wait)
+        retries += 1
